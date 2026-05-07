678d8f26d488da308007c072e3e89e8613470809
unknown <hasib.dev7@gmail.com>
2026-05-07 18:47:26 +0600

    problem 02 solve

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,6 +1,9 @@
 // Problem 01
-function filterEvenNumbers(numbers:number[]):number[]{
-    return numbers.filter((n)=>n%2===0)
+function filterEvenNumbers(numbers: number[]): number[] {
+  return numbers.filter((n) => n % 2 === 0);
 }
 
-// 
+//  Problem 02
+function reverseString(str: string): string {
+  return str.split("").reverse().join("");
+}
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,4 @@
-function filterEvenNumbers(numbers:number[]):number[]{
-    return numbers.filter((n)=>n%2===0)
+function reverseString(str: string): string {
+  return str.split("").reverse().join("");
 }
-const result=filterEvenNumbers([1, 2, 3, 4, 5, 6])
-console.log(result)
+console.log(reverseString("tpircsepyt"))
